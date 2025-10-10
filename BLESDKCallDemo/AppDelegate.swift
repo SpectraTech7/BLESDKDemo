ef5f8abf64be4dba62194a8d075b23f5393a0ca3
Spectra-iOS <apps.spectra@gmail.com>
2025-10-10 18:16:00 +0530

    update demo code.
    Adding support for background punch functionality when the app is in a minimized state.

--- BLESDKCallDemo/AppDelegate.swift
+++ BLESDKCallDemo/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import CoreLocation
+import SpectraBLEiOS
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     var window: UIWindow?
+   // var locationManager = CLLocationManager()
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        SpectraBLE.shared.locationManager.requestAlwaysAuthorization()
+        
         return true
     }
 
